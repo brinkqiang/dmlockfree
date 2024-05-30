90c644c8666dbd4c96f8b4685f8530d1254f832f
brinkqiang <brink.qiang@gmail.com>
2024-05-30 16:45:39 +0800

    add

--- test/queueexample/queueexample.cpp
+++ test/queueexample/queueexample.cpp
@@ -1,32 +1,80 @@
 #include <iostream>
-#include <dmqueue.h>
 #include <thread>
-#include <gtest.h>
+#include "gtest.h"
+#include "dmatomic_queue.h"
+#include "dmqueue.h"
+#include "dmformat.h"
 
 const int gNum = 100000000;
+const int MaxPoolSize = 100000;
 
-TEST(queueexample, queueexample)
+TEST(CDMAtomicQueue, CDMAtomicQueue)
 {
-    CDMTQueue<int> q(100000);
+	fmt::print("test CDMAtomicQueue {}\n", gNum);
+
+    CDMAtomicQueue<int> q(MaxPoolSize);
+	uint64_t total = 0;
     auto t = std::thread([&] {
-        int i = 0;
-        for (;i != gNum;)
+
+        for (int i = 1; i < gNum;)
         {
             if (q.empty())
             {
-                std::this_thread::sleep_for(std::chrono::milliseconds(1));;
+                //std::this_thread::sleep_for(std::chrono::milliseconds(1));;
                 continue;
             }
+			int *a = q.front();
             q.pop();
+
+			total += *a;
             ++i;
         }
     });
 
-    for (int i = 0; i < gNum; i++)
+    for (int i = 1; i < gNum; i++)
     {
         q.push(i);
     }
 
     t.join();
-    std::cout << "pass" << std::endl;
+	fmt::print("test CDMAtomicQueue Done total = {}\n", total);
+}
+
+TEST(CDMQueue, CDMQueue)
+{
+	fmt::print("test CDMQueue {}\n", gNum);
+
+	CDMQueue q;
+	q.Init(MaxPoolSize);
+	uint64_t total = 0;
+	auto t = std::thread([&] {
+		for (int i = 1; i < gNum;)
+		{
+			void* p = q.PopFront();
+
+			if (p == nullptr)
+			{
+				//std::this_thread::sleep_for(std::chrono::milliseconds(1));;
+				continue;
+			}
+
+			int a = reinterpret_cast<int>(p);
+
+			total += a;
+			++i;
+		}
+		});
+
+	for (int i = 1; i < gNum; )
+	{
+		if (!q.PushBack(reinterpret_cast<void*>(i)))
+		{
+			continue;
+		}
+		i++;
+	}
+
+	t.join();
+	fmt::print("test CDMQueue Done total = {}\n", total);
+
 }
