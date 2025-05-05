fe6c52ead532cc00a7bf11aeee2ea9cedb0b5155
brinkqiang <brink.qiang@gmail.com>
2025-05-05 12:20:14 +0800

    add

--- test/queueexample/queueexample.cpp
+++ test/queueexample/queueexample.cpp
@@ -11,18 +11,18 @@
 
 #include "dmformat.h"
 
-const int gNum = 1000000;       // 测试数据量调整为百万级，便于快速验证
-const int MaxPoolSize = 100000; // 根据实际需求调整
+const uint64_t gNum = 100000000;       // 测试数据量调整为百万级，便于快速验证
+const uint64_t MaxPoolSize = 100000; // 根据实际需求调整
 
 class QueueTest : public testing::Test {
 protected:
-    static const int kNum = gNum;
-    static const int kMaxPoolSize = MaxPoolSize;
+    static const uint64_t kNum = gNum;
+    static const uint64_t kMaxPoolSize = MaxPoolSize;
     uint64_t expected_total;
 
     void SetUp() override {
         // 计算期望总和: sum = 1 + 2 + ... + (kNum - 1)
-        expected_total = (static_cast<uint64_t>(kNum) - 1) * kNum / 2;
+        expected_total = (kNum - 1) * kNum / 2;
     }
 };
 
@@ -208,26 +208,27 @@ TEST_F(QueueTest, CAtomicQueue) {
     CAtomicQueue<int> q(kMaxPoolSize);
     std::atomic<uint64_t> total{0};
 
+    auto producer = std::thread([&] {
+        for (int i = 1; i < kNum;) {
+            if (q.try_push(i)) {
+                ++i;
+            }
+            else {
+                std::this_thread::yield();
+            }
+        }
+        });
+
     auto consumer = std::thread([&] {
         for (int i = 0; i < kNum - 1;) {
             if (q.empty()) {
                 std::this_thread::yield();
                 continue;
             }
-            int* val = q.front();
+            int val = *(q.front());
             q.pop();
-            total.fetch_add(*val, std::memory_order_relaxed);
+            total.fetch_add(val, std::memory_order_relaxed);
             ++i;
-        }
-    });
-
-    auto producer = std::thread([&] {
-        for (int i = 1; i < kNum;) {
-            if (q.try_push(i)) {
-                ++i;
-            } else {
-                std::this_thread::yield();
-            }
         }
     });
 
